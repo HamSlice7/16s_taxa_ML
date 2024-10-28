81f91a9f50ffba3371c2db094b12bb526e7deefa
HamSlice7 <jhambly@uoguelph.ca>
2024-10-28 16:16:00 -0400

    filter rows for more than 5% unknown nucleotides

--- filter_data.py
+++ filter_data.py
@@ -7,10 +7,17 @@ df_enterobacterales = df_from_fasta.df_from_FASTA("enterobacterales.fasta")
 df_bacillales = df_from_fasta.df_from_FASTA("bacillales.fasta")
 
 
-yes = []
+#Filter rows with more more that 5% of the sequence missing
+
+rows_to_remove_bacillales = []
+rows_to_remove_enterobacterales = []
 
 for index, row in df_bacillales.iterrows():
     if row["Nucelotide Sequence"].count("N") >= (len(row["Nucelotide Sequence"]) * 0.05):
-        yes.append("yes")
+        rows_to_remove_bacillales.append(index)
 
-print(len(yes))
+for index, row in df_enterobacterales.iterrows():
+    if row["Nucelotide Sequence"].count("N") >= (len(row["Nucelotide Sequence"]) * 0.05):
+        rows_to_remove_enterobacterales.append(index)
+
+print(len(rows_to_remove_bacillales), len(rows_to_remove_enterobacterales))
