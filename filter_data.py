8ceb2d71f4b1da1b8ab9f04091badfcb29fe33f8
HamSlice7 <jhambly@uoguelph.ca>
2024-10-29 11:16:24 -0400

    Filtering rows based on sequence length

--- filter_data.py
+++ filter_data.py
@@ -22,7 +22,7 @@ for index, row in df_enterobacterales.iterrows():
     if row["Nucelotide Sequence"].count("N") >= (len(row["Nucelotide Sequence"]) * 0.05):
         rows_to_remove_enterobacterales.append(index)
 
-print(len(rows_to_remove_bacillales), len(rows_to_remove_enterobacterales))
+#print(len(rows_to_remove_bacillales), len(rows_to_remove_enterobacterales))
 
 ##Look at the distribution of the sequence lengths
 
@@ -37,7 +37,7 @@ plt.hist(bacillales_sequence_lengths)
 plt.xlabel("Sequence Length")
 plt.ylabel("Count")
 plt.title("Frequency of the sequence of the 16S rRNA geene in Bacillales")
-plt.show()
+#plt.show()
 
 #Enterobacterales
 enterobacterales_sequence_lengths = []
@@ -50,4 +50,34 @@ plt.hist(enterobacterales_sequence_lengths)
 plt.xlabel("Sequence Length")
 plt.ylabel("Count")
 plt.title("Frequency of the sequence of the 16S rRNA geene in Enterobacterales")
-plt.show()
+#plt.show()
+
+##Removing rows where the sequence lengths are less than 90% than the rest and where the sequence lengths are greater than 90% of the rest
+
+#Filtering Bacillales data frame
+rows_to_remove_bacillales_sequence_length = []
+
+
+for index,row in df_bacillales.iterrows():
+    if len(row["Nucelotide Sequence"]) <= np.quantile(bacillales_sequence_lengths, 0.10) or len(row["Nucelotide Sequence"]) >= np.quantile(bacillales_sequence_lengths, 0.90):
+        rows_to_remove_bacillales_sequence_length.append(index)
+
+print(rows_to_remove_bacillales_sequence_length)
+print(f"Number of rows that will be removed:{len(rows_to_remove_bacillales_sequence_length)}")
+print(f"Dimensions of df_bacillales before filtering for sequence length:{df_bacillales.shape}")
+df_bacillales.drop(rows_to_remove_bacillales_sequence_length, inplace=True)
+print(f"Dimensions of df_bacillales after filtering for sequence length: {df_bacillales.shape}")
+
+#Filtering Enterobacterales data frame
+rows_to_remove_enterobacterales_sequence_length = []
+
+
+for index,row in df_enterobacterales.iterrows():
+    if len(row["Nucelotide Sequence"]) <= np.quantile(enterobacterales_sequence_lengths, 0.10) or len(row["Nucelotide Sequence"]) >= np.quantile(enterobacterales_sequence_lengths, 0.90):
+        rows_to_remove_enterobacterales_sequence_length.append(index)
+
+print(rows_to_remove_enterobacterales_sequence_length)
+print(f"Number of rows that will be removed:{len(rows_to_remove_enterobacterales_sequence_length)}")
+print(f"Dimensions of df_enterobacterales before filtering for sequence length:{df_enterobacterales.shape}")
+df_enterobacterales.drop(rows_to_remove_enterobacterales_sequence_length, inplace=True)
+print(f"Dimensions of df_enterobacterales after filtering for sequence length: {df_enterobacterales.shape}")
