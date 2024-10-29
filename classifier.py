8816067f05debd9bb7c9e3b2edbd87a6a5591b0e
HamSlice7 <jhambly@uoguelph.ca>
2024-10-29 12:24:03 -0400

    Randomly sampling 544 rows for Bacillales to balance data set and adding binary labels for gram positive and gram negative

--- classifier.py
+++ classifier.py
@@ -6,3 +6,12 @@ print(df_bacillales.shape)
 
 df_enterobacterales = pd.read_csv("filtered_enterobacterales.csv")
 print(df_enterobacterales.shape)
+
+#Balance the two data sets by randomly sampling 544 rows from df_bacillales
+df_bacillales = df_bacillales.sample(n=544, random_state=25).reset_index(drop=True)
+print(df_bacillales.shape)
+print(df_bacillales.head())
+
+#Add binary labels for gram positive and gram negative
+df_bacillales["Label"] = 1
+df_enterobacterales["Label"] = 0
