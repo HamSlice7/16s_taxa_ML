c815d1851197cde99ca08b0c20ebe30f3db1f353
HamSlice7 <jhambly@uoguelph.ca>
2024-10-31 16:30:08 -0400

    Assessing the performance of Random Forest Classifier algo for building a model to classify Enterbacterales and Bacillales using cross validation

--- classification.py
+++ classification.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import KFold
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
 
 #Importing the csv file containing the kmer features for each sequence
 df = pd.read_csv("data_with_2kmer_features.csv")
@@ -15,3 +17,14 @@ y = df["Label"]
 
 #Saving the features for each sequence as 'x'
 x = df.iloc[0:df.shape[0],1:df.shape[1]]
+
+for training_index, test_index in kf.split(df):
+    x_train, x_test = x.iloc[training_index], x.iloc[test_index]
+    y_train, y_test = y.iloc[training_index], y.iloc[test_index]
+
+    RF_classifier = RandomForestClassifier(random_state=0)
+    RF_classifier.fit(x_train, y_train)
+    y_pred = RF_classifier.predict(x_test)
+
+    print("Accuracy of the model:", metrics.accuracy_score(y_test, y_pred))
+    print(metrics.confusion_matrix(y_test,y_pred))
