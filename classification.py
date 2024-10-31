3bb7a0955236a97e0745045bb2284da1ee68645b
HamSlice7 <jhambly@uoguelph.ca>
2024-10-31 16:36:48 -0400

    Adding comments

--- classification.py
+++ classification.py
@@ -18,13 +18,21 @@ y = df["Label"]
 #Saving the features for each sequence as 'x'
 x = df.iloc[0:df.shape[0],1:df.shape[1]]
 
+#Using 5-fold cross validation to assess the performance of using the Random Forest algo to build a classifier model using different parts of the data
 for training_index, test_index in kf.split(df):
+    #Splitting the data into training and testing sets for each iteration of CV
     x_train, x_test = x.iloc[training_index], x.iloc[test_index]
     y_train, y_test = y.iloc[training_index], y.iloc[test_index]
 
+    #Creating a RF Classifier object
     RF_classifier = RandomForestClassifier(random_state=0)
+
+    #Training a random forest model using the training data
     RF_classifier.fit(x_train, y_train)
+
+    #Prediciting the class for the test set
     y_pred = RF_classifier.predict(x_test)
 
+    #Assessing the performance of the model
     print("Accuracy of the model:", metrics.accuracy_score(y_test, y_pred))
     print(metrics.confusion_matrix(y_test,y_pred))
